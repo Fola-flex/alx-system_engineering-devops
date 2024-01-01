17117d0990c41e5f4066bfbd26ed8872056ed5ff
Muiz <folarinolaore93@gmail.com>
2024-01-01 15:54:11 -0800

    Conformed files with puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Creates a file in /tmp
 
-file { 'newFile':
-  path => '/tmp/school',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+file { '/tmp/school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-# Installing flask from pip3 using Puppet
+#!/usr/bin/pup
+#Installing flask from pip3 using Puppet
 
 package { 'flask':
-  ensure => '2.1.0',
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
