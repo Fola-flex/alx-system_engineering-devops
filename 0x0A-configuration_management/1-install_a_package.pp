e2dcec97fe03d60c77f599eeae50811b2b9ee83d
Muiz <folarinolaore93@gmail.com>
2024-01-01 16:36:41 -0800

    Corrected file 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
 #!/usr/bin/pup
-# Installing flask from pip3 using Puppet
+# Installing flask 2.1.0 from pip3 using Puppet
 
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3'
 }
